67481d22bc8dde8afd714daf77c47be416d234c2
Sarah B <sarahbinaz1020@gmail.com>
2021-03-22 10:04:07 -0700

    added info for classes

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 // inquirer info/questions
 const fs = require("fs");
 const inquirer = require("inquirer");
+const Manager = require("./lib/Manager");
 
+const employees = [];
+
+inquirer.prompt([{
+    name: "newEmployee",
+    message: "What employee would you like to add?",
+    type: "list",
+    choices: ["Engineer", "Intern", "I'm done],"]
+}])
+
+.then(function ({ employee}) {
+
+    console.log(response);
+    switch (employee) {
+        case "Engineer":
+        // run engineer prompt
+        break;
+        case "Intern":
+            // Run intern prompt
+        case "I'm done":
+            // generate the html file
+            break;
+    }
+}
+)
+
+inquirer.prompt (questions)
 // what is your role?
 const questions = [
     {
@@ -10,7 +37,23 @@ const questions = [
         name: 'role',
         choices: ["Manager", "Engineer", "Intern"],
     }
+
 ]
 // then sub questions
+// function getSubQuestions (data){
+//     const roleChoice = data.role;
+//     let role;
+//     switch (roleChoice) {
+//         case "Manager":
+//             role = 
+//     }
+// }
 
 // new - to make a real Manager
+
+// function askMenu(){
+//     prompt(menu)
+//     .then(data => {
+
+//     })
+// }
